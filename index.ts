7616c3d4097a4276b05528af67a38dfbea894e45
chris <47784402+christhefrog@users.noreply.github.com>
2025-02-17 11:50:06 +0100

    Added an emoji to the response

--- index.ts
+++ index.ts
@@ -2,5 +2,5 @@
 import {Request, Response} from "express";
 
 export const get = async (req : Request, res : Response) => { 
-    return res.status(200).json({ping: "pong"})
+    return res.status(200).json({ping: "pong 🏓"})
  }
